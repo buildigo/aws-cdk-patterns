162779803f992fa794ebaa3e32289f3a2baaf456
Mathieu Meylan <mathieu.meylan@buildigo.ch>
2023-07-26 14:31:34 +0200

    fix: fix error when collecting coverage

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
 const path = require('path')
 
 module.exports = {
-  displayName: 'aws-cdk-pattens',
-  roots: [path.join(__dirname, 'src')],
-  testMatch: ['<rootDir>/**/*.(test).ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transform: {
-    '^.+\\.ts': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.cjs.json',
+    displayName: 'aws-cdk-pattens',
+    roots: [path.join(__dirname, 'src')],
+    testMatch: ['<rootDir>/**/*.(test).ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+        '^.+\\.ts': 'ts-jest',
     },
-  },
-  watchPlugins: ['jest-watch-select-projects'],
-  collectCoverageFrom: ['src/**/*.ts'],
-  testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.cjs.json',
+        },
+    },
+    watchPlugins: ['jest-watch-select-projects'],
+    testEnvironment: 'node',
 }
