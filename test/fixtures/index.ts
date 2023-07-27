e896d6026cfdf3599d18118dc52608983ada4223
Mathieu Meylan <mathieu.meylan@buildigo.ch>
2023-07-27 16:25:41 +0200

    chore: add support for "withoutEnlargement" and tests

---
+++ test/fixtures/index.ts
@@ -0,0 +1,10 @@
+import {join} from 'path'
+import {readFile} from 'fs/promises'
+
+function getPath(filename: string) {
+  return join(__dirname, filename)
+}
+
+export const images = {
+  inputJpeg: () => readFile(getPath('2569067123_aca715a2ee_o.jpg')),
+}
