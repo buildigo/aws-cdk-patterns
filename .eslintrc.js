4c9dd5be0ba3c3a20394211e5f01186e744c263f
Mathieu Meylan <mathieu.meylan@hey.com>
2023-11-14 20:29:55 +0100

    chore: remove unnecessary package

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,20 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-    }
+    },
+    overrides: [
+        {
+            files: ['*.ts'],
+            extends: ['@buildigo/eslint-config-typescript'],
+            settings: {
+                'import/resolver': {
+                    typescript: {
+                        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/aws-lambda`
+                        project: '<root>/tsconfig.base.json',
+                    },
+                },
+            },
+        },
+    ],
+
 }
